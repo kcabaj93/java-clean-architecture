27501f0eeb3b13337dce17ad01098e00859dde24
Kamil Cabaj <k.cabaj@globalsystem.pl>
2021-06-06 22:45:32 +0200

    dodanie fabryki tasków

---
+++ src/main/java/io/github/mat3e/task/TaskFactory.java
@@ -0,0 +1,15 @@
+package io.github.mat3e.task;
+
+import io.github.mat3e.project.Project;
+import org.springframework.stereotype.Service;
+
+@Service
+class TaskFactory {
+    Task from(final TaskDto source, final Project project) {
+        var result =  new Task(source.getDescription(), source.getDeadline(), project);
+        result.setId(source.getId());
+        result.setDone(source.isDone());
+        result.setAdditionalComment(source.getAdditionalComment());
+        return result;
+    }
+}
